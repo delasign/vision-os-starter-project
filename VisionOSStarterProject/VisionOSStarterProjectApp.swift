30bc246eca5e566dfa9eef13bf24ed04b6f1c992
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-08 11:43:06 -0400

    Initial Commit

---
+++ VisionOSStarterProject/VisionOSStarterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSStarterProjectApp.swift
+//  VisionOSStarterProject
+//
+//  Created by Oscar de la Hera Gomez on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSStarterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
