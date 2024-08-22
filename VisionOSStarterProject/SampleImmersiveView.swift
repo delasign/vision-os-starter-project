6fad6db4f11b5ab5e7f9aa70aea9c2ef5e0c5c80
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-22 12:08:40 -0400

    Complete

--- VisionOSStarterProject/SampleImmersiveView.swift
+++ VisionOSStarterProject/SampleImmersiveView.swift
@@ -9,10 +9,38 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+@MainActor
 struct SampleImmersiveView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
+            RealityView { content in
+                if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                    // Set its Position
+                    scene.position = SIMD3<Float>(0, 1, -1)
+                    // Set its Scale
+                    scene.scale = SIMD3<Float>(2,2,2)
+                    content.add(scene)
+                    
+                    // Determine the transform to animate to.
+                    var transform = scene.transform
+                    let yAxis = SIMD3<Float>(0, 1, 0) // Y-axis
+                    let rotationAmount = simd_quatf(angle: .pi, axis: yAxis)
+                    transform.rotation *= rotationAmount
+                    
+                    // Create the rotate on its y axis animation
+                    let rotateOnItsYAxisAnimation = FromToByAnimation(from: scene.transform,
+                                                                      to:transform,
+                                                                        duration: 2,
+                                                                        timing: .linear,
+                                                                    bindTarget: .transform,
+                                                                      repeatMode: .repeat)
+                    
+                    // Set the Animation
+                    if let animation = try? AnimationResource.generate(with: rotateOnItsYAxisAnimation) {
+                        scene.playAnimation(animation)
+                    }
+                }
+            }
         }
     }
 }
