195e9f2fa41f3589280ed61776120f3da3a2795c
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-27 14:15:51 -0400

    Complete

--- VisionOSStarterProject/ContentView.swift
+++ VisionOSStarterProject/ContentView.swift
@@ -12,8 +12,28 @@ import RealityKitContent
 struct ContentView: View {
     var body: some View {
         RealityView { content in
-            if let fire = try? await Entity(named: "Fire", in: realityKitContentBundle) {
-                content.add(fire)
+            if let element = try? await Entity(named: "Shaded_Bread", in: realityKitContentBundle) {
+                
+                // Add the Scene
+                content.add(element)
+                
+                guard let root = element.children.first else { return }
+                
+                // Gather the bread
+                guard let brioche = root.children.first(where: { $0.name == "Brioche_Bread"}) else { return }
+                
+                // Get its component
+                guard var modelComponent = brioche.components[ModelComponent.self],
+                      var shaderGraphMaterial = modelComponent.materials.first as? ShaderGraphMaterial else { return }
+                
+                // Alter the shader graph parameter
+                do {
+                    try shaderGraphMaterial.setParameter(name: "Constant", value: .float(10))
+                                modelComponent.materials = [shaderGraphMaterial]
+                    brioche.components.set(modelComponent)
+                } catch {
+                    
+                }
             }
         }
     }
